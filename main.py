380955c7c10dc4de8640fa4143643f83573a1b02
Nurrrdaulet <damnanimeshnik@gmail.com>
2025-03-08 12:36:04 +0500

    Worker -> web service

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import asyncio
 import logging
 import sys
 from os import getenv
+from fastapi import FastAPI
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -11,8 +11,6 @@ from handlers.text_handler import router as text_router
 from handlers.image_handler import router as image_router
 from handlers.math_handler import router as math_router
 
-# Получается роутеры прилегают to диспатчеру. Диспатчер типа центр, хаб, а хендлеры как источники
-
 load_dotenv()
 TOKEN = getenv("TOKEN")
 
@@ -20,8 +18,10 @@ TOKEN = getenv("TOKEN")
 logging.basicConfig(level=logging.INFO)
 
 dp = Dispatcher()
+app = FastAPI()
 
-async def main() -> None:
+@app.on_event("startup")
+async def on_startup():
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp.include_router(start_router)
@@ -31,7 +31,11 @@ async def main() -> None:
     # And the run events dispatching
     await dp.start_polling(bot)
 
+@app.get("/")
+async def read_root():
+    return {"message": "Bot is running"}
 
 if __name__ == "__main__":
+    import uvicorn
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    uvicorn.run(app, host="0.0.0.0", port=8000)
