d4beff5c314733bd5c3dcde89b70be592d2ae441
Nurrrdaulet <damnanimeshnik@gmail.com>
2025-03-08 12:53:15 +0500

    uvicorn added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
+from contextlib import asynccontextmanager
 from handlers.start_handler import router as start_router
 from handlers.text_handler import router as text_router
 from handlers.image_handler import router as image_router
@@ -17,25 +18,38 @@ TOKEN = getenv("TOKEN")
 # turn on logging
 logging.basicConfig(level=logging.INFO)
 
-dp = Dispatcher()
-app = FastAPI()
 
-@app.on_event("startup")
-async def on_startup():
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
+# Создаем FastAPI приложение с использованием lifespan
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Инициализация бота и диспетчера
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+
+    # Подключаем роутеры
     dp.include_router(start_router)
     dp.include_router(math_router)
     dp.include_router(text_router)
     dp.include_router(image_router)
-    # And the run events dispatching
+
+    # Запуск бота
     await dp.start_polling(bot)
+
+    yield  # Код для завершения работы можно вставить сюда
+
+
+# Создаем объект FastAPI с использованием lifespan
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def read_root():
     return {"message": "Bot is running"}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Запуск FastAPI через Uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
